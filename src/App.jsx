ae96b4611213de7151e7884cc2042a577075e18b
amtaha718 <a.m.taha718@gmail.com>
2025-05-30 13:48:43 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,43 @@ import './index.css';
 const puzzles = [
   {
     fen: 'r1bqkbnr/ppp2ppp/2n5/1B1pp3/3PP3/5N2/PPP2PPP/RNBQK2R w KQkq - 0 4',
-    moves: ['e4d5', 'd8d5', 'b1c3', 'd5a5', 'c1d2', 'f8b4']
+    moves: ['e4d5', 'd8d5', 'b1c3', 'd5a5']
+  },
+  {
+    fen: 'r1bqkbnr/pp3ppp/2n1p3/2pp4/3PP3/2N2N2/PPP2PPP/R1BQKB1R w KQkq - 0 5',
+    moves: ['e4d5', 'e6d5', 'f1b5', 'g8f6']
+  },
+  {
+    fen: 'rnbqkb1r/pp1ppppp/5n2/2p5/2P5/5NP1/PP1PPP1P/RNBQKB1R w KQkq - 0 3',
+    moves: ['f1g2', 'b8c6', 'd2d4', 'c5d4']
+  },
+  {
+    fen: 'r1bqk1nr/pppp1ppp/2n5/4p3/1b1PP3/5N2/PPPN1PPP/R1BQKB1R w KQkq - 0 4',
+    moves: ['d4e5', 'c6e5', 'f3e5', 'd7d6']
+  },
+  {
+    fen: 'r2qkbnr/ppp2ppp/2n1b3/3p4/3P4/2P2N2/PP2PPPP/RNBQKB1R w KQkq - 2 5',
+    moves: ['c1g5', 'f8e7', 'g5e7', 'c6e7']
+  },
+  {
+    fen: 'rnbqkb1r/pp3ppp/2p1pn2/3p4/3P4/2N1PN2/PP3PPP/R1BQKB1R w KQkq - 0 5',
+    moves: ['f1d3', 'f8d6', 'e3e4', 'd5e4']
+  },
+  {
+    fen: 'rnbqkbnr/pp3ppp/4p3/2ppP3/8/5N2/PPP2PPP/RNBQKB1R w KQkq - 0 5',
+    moves: ['c2c3', 'b8c6', 'd2d4', 'c5d4']
+  },
+  {
+    fen: 'r1bq1rk1/pppp1ppp/2n2n2/4p3/2B1P3/2NP1N2/PPP2PPP/R1BQ1RK1 w - - 0 6',
+    moves: ['c1g5', 'h7h6', 'g5h4', 'd7d6']
+  },
+  {
+    fen: 'r2qk2r/ppp2ppp/2n2n2/2bp4/4P3/1NN2P2/PPP3PP/R1BQ1RK1 w kq - 0 9',
+    moves: ['e4d5', 'f6d5', 'c1g5', 'd8g5']
+  },
+  {
+    fen: 'r1bqkb1r/pp2pppp/2n2n2/2pp4/3P4/2N1PN2/PP3PPP/R1BQKB1R w KQkq - 0 5',
+    moves: ['f1d3', 'c8g4', 'd1b3', 'c5c4']
   },
 ];
 

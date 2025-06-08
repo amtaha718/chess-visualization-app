f1cb8cc0eb10024bd8924019b541ff6ac9bc175b
amtaha718 <a.m.taha718@gmail.com>
2025-06-07 23:06:46 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -37,15 +37,15 @@ const loadSessionData = () => {
   }
 };
 
-// Icon Components (SVG) - Smaller sizes
+// Icon Components (SVG) - 50% larger
 const PrevIcon = () => (
-  <svg width="16" height="16" viewBox="0 0 24 24" fill="black">
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="black">
     <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"/>
   </svg>
 );
 
 const PlayIcon = ({ isPlaying }) => (
-  <svg width="18" height="18" viewBox="0 0 24 24" fill="black">
+  <svg width="27" height="27" viewBox="0 0 24 24" fill="black">
     {isPlaying ? (
       <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/>
     ) : (
@@ -55,26 +55,26 @@ const PlayIcon = ({ isPlaying }) => (
 );
 
 const HintIcon = () => (
-  <svg width="16" height="16" viewBox="0 0 24 24" fill="black">
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="black">
     <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7z"/>
     <circle cx="12" cy="9" r="1" fill="white"/>
   </svg>
 );
 
 const RevealIcon = () => (
-  <svg width="18" height="18" viewBox="0 0 24 24" fill="black">
+  <svg width="27" height="27" viewBox="0 0 24 24" fill="black">
     <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm-1 13h2v-6h-2v6zm0-8h2V5h-2v2z"/>
   </svg>
 );
 
 const GearIcon = () => (
-  <svg width="16" height="16" viewBox="0 0 24 24" fill="black">
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="black">
     <path d="M12 15.5A3.5 3.5 0 0 1 8.5 12A3.5 3.5 0 0 1 12 8.5a3.5 3.5 0 0 1 3.5 3.5a3.5 3.5 0 0 1-3.5 3.5m7.43-2.53c.04-.32.07-.64.07-.97c0-.33-.03-.66-.07-1l2.11-1.63c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.31-.61-.22l-2.49 1c-.52-.39-1.06-.73-1.69-.98l-.37-2.65A.506.506 0 0 0 14 2h-4c-.25 0-.46.18-.5.42l-.37 2.65c-.63.25-1.17.59-1.69.98l-2.49-1c-.22-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64L4.57 11c-.04.34-.07.67-.07 1c0 .33.03.65.07.97l-2.11 1.66c-.19.15-.25.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1.01c.52.4 1.06.74 1.69.99l.37 2.65c.04.24.25.42.5.42h4c.25 0 .46-.18.5-.42l.37-2.65c.63-.26 1.17-.59 1.69-.99l2.49 1.01c.22.08.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.66Z"/>
   </svg>
 );
 
 const NextIcon = () => (
-  <svg width="16" height="16" viewBox="0 0 24 24" fill="black">
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="black">
     <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/>
   </svg>
 );
